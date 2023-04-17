2b06dce6b5bb75c99b856f9058ae3b82feb3a2ef
Aniwesh20 <96642963+Aniwesh20@users.noreply.github.com>
2023-04-17 21:50:21 +0530

    Add files via upload

---
+++ Klargestelement.java
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void Klargestelement(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+   
+    
+    Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int a[]=new int[n];
+        
+        
+        for(int i=0;i<a.length;i++){
+            a[i]=scn.nextInt();
+        }
+        int k=scn.nextInt();
+        
+        
+        Arrays.sort(a);
+        
+        System.out.println(a[a.length-k]);
+    
+    }
+}
