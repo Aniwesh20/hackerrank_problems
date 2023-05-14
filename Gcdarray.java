e671660557fc5a391dda40dddd05115c401d1bee
Aniwesh20 <96642963+Aniwesh20@users.noreply.github.com>
2023-05-14 21:09:36 +0530

    Add files via upload

---
+++ Gcdarray.java
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.io.*;
+import java.util.*;
+public class Gcdarray {
+    static int gcd(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        return gcd(b , a%b);
+    }
+    
+    static int findGCD(int arr[], int n)
+    {
+        int result = arr[0];
+        for (int element: arr){
+            result = gcd(result, element);
+ 
+            if(result == 1)
+            {
+               return 1;
+            }
+        }
+ 
+        return result;
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn=new Scanner(System.in);
+        int n= scn.nextInt();
+        
+        int arr[]=new int[n];
+        
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+
+        System.out.println(findGCD(arr,n));
+    }
+}
